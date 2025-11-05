9aeb241d6dc103480a63340331659c7c0859524d
lchampicas <lchampicas@educacion.navarra.es>
2025-11-05 10:40:09 +0100

    Feat: Añadir funcionalidad de contador de matches con sus estilos correspondientes

--- js/app.js
+++ js/app.js
@@ -6,14 +6,32 @@ const btnRecargar = document.getElementById("btn_recargar");
 const btnMatch = document.getElementById("btn_match");
 const btnNoMatch = document.getElementById("btn_nomatch");
 
+const matches = document.querySelector(".matches");
+
 pets = Array.from(pets);
 
 console.log(pets);
 
+document.addEventListener("click", (e) => {
+  if (e.target === btnMatch) {
+    let spanContador = document.querySelector(".match-count");
+    let contador = 1;
+    if (spanContador) {
+      contador = parseInt(spanContador.textContent) + 1;
+      spanContador.textContent = contador;
+    } else {
+      spanContador = document.createElement("span");
+      spanContador.classList.add("match-count");
+      spanContador.textContent = 1;
+      matches.appendChild(spanContador);
+    }
+    console.log(contador);
+  }
+});
 
-document.addEventListener("click", e => {
+/*document.addEventListener("click", e => {
     if(e.target === btnRecargar || e.target === btnMatch || e.target === btnNoMatch){
         pets.pop().remove();
         console.log(pets);
     }
-} )
+} )*/
